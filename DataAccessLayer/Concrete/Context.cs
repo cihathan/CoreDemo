3f6d9e51eb1fe3aaf478ff190ed39bc074415296
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2023-01-12 17:30:37 +0300

    References Defined and Context Dbset Properties created.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=HAN\\MSQLSERVER01;database=CoreBlogDb;integrated security=true;");          
+        }
+        public DbSet<About> abouts { get; set; }
+        public DbSet<Blog> blogs { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Comment> comments { get; set; }
+        public DbSet<Contact> contacts { get; set; }
+        public DbSet<Writer> writers { get; set; }
+    }
+}
