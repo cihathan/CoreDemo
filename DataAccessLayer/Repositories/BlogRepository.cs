727afc26d2807e5de395966ca32afae2eb93fd86
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2023-01-13 19:48:33 +0300

    Blog and Category repository class created.

---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Add(blog);
+            context.SaveChanges();
+        }
+
+        public void DeleteBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Remove(blog);
+            context.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            using var context = new Context();
+            return context.Blogs.Find(id);
+        }
+
+        public List<Blog> ListAllBlog()
+        {
+            using var context = new Context();
+            return context.Blogs.ToList();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Update(blog);
+            context.SaveChanges();
+        }
+    }
+}
