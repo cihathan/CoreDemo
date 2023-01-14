f6bdbf5ed8c998556c2606473030fcc474724d75
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2023-01-14 23:05:26 +0300

    Service interfaces and manager classes were created in the business layer. The Entity Framework repository has been created. The category manager class has been revised.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository efCategoryRepository;
+
+        public CategoryManager()
+        {
+            efCategoryRepository= new EfCategoryRepository();
+        }
+        public void AddCategory(Category category)
+        {
+            efCategoryRepository.Insert(category);
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            efCategoryRepository.Delete(category);
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return efCategoryRepository.GetAll();
+        }
+
+        public Category GetById(int id)
+        {
+            return efCategoryRepository.GetById(id);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            efCategoryRepository.Update(category);
+        }
+    }
+}
