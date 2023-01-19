cea05870bb972c77979301f40de4e6b16a9192b0
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2023-01-19 18:23:34 +0300

    Listing of comments for blogs.

---
+++ CoreDemo/ViewComponents/Comment/CommentListBlog.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+	public class CommentListBlog:ViewComponent
+	{
+		CommentManager commentManager = new CommentManager(new EfCommentRepository());
+		public IViewComponentResult Invoke()
+		{
+			var values = commentManager.GetAllComments(4);
+			return View(values);
+		}
+	}
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentvalues = new List<UserComment>
+			{
+				new UserComment
+				{
+					Id= 1,
+					UserName="Han"
+				},
+				new UserComment
+				{
+					Id= 2,
+					UserName="Berkcan"
+				},
+				new UserComment
+				{
+					Id= 3,
+					UserName="Devrim"
+				}
+			};
+			return View(commentvalues);
+		}
+	}
+}
