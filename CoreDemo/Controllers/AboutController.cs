c0b6b9dbca9f05291d72f52994654334345fcf75
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2023-01-26 21:49:32 +0300

    Added About and Contact Page.

--- CoreDemo/Controllers/AboutController.cs
+++ CoreDemo/Controllers/AboutController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
-	public class AboutController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+
+        public IActionResult Index()
+        {
+            var values = aboutManager.GetAllAbouts();
+            return View(values);
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
 }
---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoreDemo.Controllers
+{
+	public class ContactController : Controller
+	{
+		ContactManager contactManager = new ContactManager(new EfContactRepository());
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+			contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+			contact.ContactStatus = true;
+			contactManager.AddContact(contact);
+			return RedirectToAction("Index", "Blog");
+        }
+    }
+}
