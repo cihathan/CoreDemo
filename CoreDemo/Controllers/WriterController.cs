fc852265c406667b886eb5b5ad9b4439853b6e3b
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2023-01-28 00:03:19 +0300

    A minor update for the UI design.

---
+++ CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
