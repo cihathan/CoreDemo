db2a592e1499d85a1d60f005ba09f1dd9778e6b7
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2023-01-29 00:58:02 +0300

    Login and Authorize Process.

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,13 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Concrete;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
     public class LoginController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
-        
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index(Writer writer)
+        {
+            Context context = new Context();
+            //var datavalue = context.Writers.FirstOrDefault(x=>x.WriterMail == writer.WriterMail && x.WriterPassword== writer.WriterPassword);
+            //if (datavalue != null)
+            //{
+            //    HttpContext.Session.SetString("UserName", writer.WriterMail);
+            //    return RedirectToAction("Index", "Writer");
+            //}
+            //else
+            //{
+            //    return View();
+            //}
+            var datavalue = context.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
+            if (datavalue!=null)
+            {
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name,writer.WriterMail)
+                };
+                var useridentity = new ClaimsIdentity(claims,"value");
+                ClaimsPrincipal principal= new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Writer");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
     }
 }
--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class WriterController : Controller
     {
+        
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
         {
             return View();
         }
