bf1fb4ca4024da560f6cc05384ef8cd9858c716b
Cihat Han Toparlak <cihathantoparlak42@gmail.com>
2023-01-31 03:08:11 +0300

    Author Panel UI design
    Added Author Panel Theme.
    Author Panel Navbar has been edited.

---
+++ CoreDemo/ViewComponents/WriterNotification.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+    public class WriterNotification : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
